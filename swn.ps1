611e41a07eb483a9ea68ef19d24bf07b7c438e5b
Walnit <39372439+Walnit@users.noreply.github.com>
2024-03-27 08:36:50 +0800

    [v0.1.2] Update Windows script to make it silent

--- swn.ps1
+++ swn.ps1
@@ -7,7 +7,7 @@ if (Test-Path "~/.swnconfig" -PathType Leaf) {
 		$PASSWORD = $CONFIG_CONTENT[1]
 		$GOOGLE_TEST_OUTPUT -match 'https:\/\/.{50}'
 		$LOGIN_URL = $Matches[0]
-		curl.exe -s $LOGIN_URL --ssl-revoke-best-effort
+		curl.exe -s $LOGIN_URL --ssl-revoke-best-effort -s
 		$LOGIN_URL -match '\?.{16}'
 		$MAGIC = ($Matches[0])[-16..-1] -join ''
 		curl.exe --data-urlencode username=$USERNAME --data-urlencode password=$PASSWORD --data-urlencode magic=$MAGIC $LOGIN_URL --ssl-revoke-best-effort -s
